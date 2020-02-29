a5549e81594edbe240e8b84c2b93af778f99b9e1
Tahmina <58538187+tahmina11@users.noreply.github.com>
2020-02-29 14:57:22 -0500

    Update LocationMap.java

--- src/test/LocationMap.java
+++ src/test/LocationMap.java
@@ -3,6 +3,6 @@ package test;
 public class LocationMap {
     public static void main(String[] args) {
         System.out.println("heelllloooo world we are testing here");
-
+         //from Github webSite with love <3
     }
 }
