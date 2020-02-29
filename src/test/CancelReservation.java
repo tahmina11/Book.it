80ae410bd66b560030acd4b637b836f12f1e2c2b
Tahmina <Tahmina@Tahminas-MacBook-Air.local>
2020-02-29 14:36:32 -0500

    add CancelReservation

---
+++ src/test/CancelReservation.java
@@ -0,0 +1,8 @@
+package test;
+
+public class CancelReservation {
+    public static void main(String[] args) {
+        System.out.println("Java should be fun, but lets start with selenium");
+
+    }
+}
