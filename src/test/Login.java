2203eb994a14c6706a17679537dacd47f60680ce
Tahmina <Tahmina@Tahminas-MacBook-Air.local>
2020-02-29 14:18:08 -0500

    add Login.java

---
+++ src/test/Login.java
@@ -0,0 +1,8 @@
+package test;
+
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("Life is so good, we are having new project and name is Bookit! and it is fun!" +
+                "we are creating new repository for the project, cuz out git hub is not containing it yet.");
+    }
+}
